8a921b5d3362526093868de454b8a022e3efb64d
ShoushanikBabaian <68646681+ShoushanikBabaian@users.noreply.github.com>
2020-07-31 21:00:04 +0400

    Update homework.py

--- homework.py
+++ homework.py
@@ -43,10 +43,3 @@ print(list1)
 
 list1 = [5, 20, 15, 20, 25, 50, 20]
 
-
-def removeValue(sampleList, val):
-    return [value for value in sampleList if value != val]
-
-
-resList = removeValue(list1, 20)
-print(resList)
